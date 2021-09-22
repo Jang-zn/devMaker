544cb90e13e4577323896501e88335f79b71efc2
Jang <zn2309@naver.com>
2021-09-23 01:08:21 +0900

    controller에 createDeveloper mapping

--- src/main/java/com/study/devmaker/controller/DMakerController.java
+++ src/main/java/com/study/devmaker/controller/DMakerController.java
@@ -1,21 +1,31 @@
 package com.study.devmaker.controller;
 
 
+import com.study.devmaker.service.DmakerService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 @Slf4j
 @RestController
+@RequiredArgsConstructor
 public class DMakerController {
-    @GetMapping("/devs")
+
+    private final DmakerService dmakerService;
+
+    @GetMapping("/create-devs")
     public List<String> getAllDevelopers(){
         //GET /devs HTTP/1.1 요청이 오면 이게 응답함
         log.info("GET /devs HTTP/1.1");
-        return Arrays.asList("1","2","3");
+        dmakerService.createDeveloper();
+        return Collections.singletonList("Jang");
     }
 
 
