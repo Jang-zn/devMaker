bbfaaf8399ad1d75f6c41ee3c9a841216d010c2b
Jang <zn2309@naver.com>
2021-09-27 13:35:46 +0900

    Junit Test  - 근데 이렇게 작성하면 단위테스트의 격리성이 낮으니까 Mockito를 사용하자    @SpringBootTest -> @ExtendWith(MockitoExtension.class)    서비스 객체에 필요한 Repository를 @Mock으로 멤버변수에 지정해준다.    이후 @AutoWired -> @InjectMocks 으로 Service 객체에 필요한 Mock을 주입해줌
     - Test할 메소드에선 Mock들의 동작을 지정해준다

--- src/test/java/com/study/devmaker/service/DmakerServiceTest.java
+++ src/test/java/com/study/devmaker/service/DmakerServiceTest.java
@@ -1,37 +1,55 @@
 package com.study.devmaker.service;
 
 import com.study.devmaker.dto.CreateDeveloperDto;
+import com.study.devmaker.dto.DeveloperDetailDto;
 import com.study.devmaker.dto.DeveloperDto;
 import com.study.devmaker.entity.Developer;
+import com.study.devmaker.repository.DevRepository;
+import com.study.devmaker.repository.RetiredDevRepository;
 import com.study.devmaker.type.DeveloperLevel;
 import com.study.devmaker.type.DeveloperSkillType;
+import com.study.devmaker.type.StatusCode;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
 
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 class DmakerServiceTest {
+    @Mock
+    private DevRepository devRepository;
 
-    @Autowired
+    @Mock
+    private RetiredDevRepository retiredDevRepository;
+
+    @InjectMocks
     DmakerService dmakerService;
 
     @Test
     public void test01(){
-        dmakerService.createDeveloper(CreateDeveloperDto.Request.builder()
-                .developerLevel(DeveloperLevel.SENIOR)
-                .developerSkillType(DeveloperSkillType.FULLSTACK)
-                .experienceYears(15)
-                .age(40)
-                .name("J")
-                .memberId("zn2309")
-                .build());
-        List<DeveloperDto> allEmployedDevelopers = dmakerService.getAllEmployedDevelopers();
-        System.out.println("-------------------");
-        System.out.println(allEmployedDevelopers);
-        System.out.println("-------------------");
+        given(devRepository.findByMemberId(anyString())).willReturn(Optional.of(Developer.builder()
+                        .developerLevel(DeveloperLevel.SENIOR)
+                        .developerSkillType(DeveloperSkillType.FULLSTACK)
+                        .experienceYears(15)
+                        .memberId("zn2309")
+                        .name("J")
+                        .age(35)
+                        .statusCode(StatusCode.EMPLOYED)
+                        .build()));
+
+        DeveloperDetailDto developerDetailDto = dmakerService.getDeveloper("memberId");
+        assertEquals(DeveloperLevel.SENIOR, developerDetailDto.getDeveloperLevel());
+        assertEquals(DeveloperSkillType.FULLSTACK, developerDetailDto.getDeveloperSkillType());
+        assertEquals(15, developerDetailDto.getExperienceYears());
     }
 }
